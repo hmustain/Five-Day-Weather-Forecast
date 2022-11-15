91b90e754de5474270f05bc9b49067b9c30f1053
hmustain <hunter@bunkerbranding.com>
2022-11-15 14:30:31 -0600

    added var apiKey and my apikey inside of it for later

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#cityname");
 var searchCityContainerEl = document.querySelector("#searchcity");
+var apiKey = "1c0a9400ca0f8243bdd42c0e2c421139";
+
 
 // working on event listener and local storage to store city's searched
 $( "#citysearchbtn" ).on("click", function(event){
@@ -8,6 +10,14 @@ $( "#citysearchbtn" ).on("click", function(event){
 
     var cityEntered = $("#cityname").val().trim();
 
+    if (cityEntered) {
+        console.log("the city you entered is " + cityEntered)
+        
+    }
+    else {alert("Please enter a valid City")};
+
     localStorage.setItem("city name", JSON.stringify(cityEntered));
     console.log("the city you entered is " + cityEntered);
 })
+
+console.log("my api key is " + apiKey);
