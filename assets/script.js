dd7b519df1c0c86d7d97bdd19e06fd21149cb1da
hmustain <hunter@bunkerbranding.com>
2022-11-15 12:46:13 -0600

    saving work, city input is now being saved to local storage

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#cityname");
+var searchCityContainerEl = document.querySelector("#searchcity");
+
+var citySearch = [];
+// working on event listener and local storage to store city's searched
+$( "#citysearchbtn" ).on("click", function(event){
+    event.preventDefault();
+
+    var cityEntered = $("#cityname").val().trim();
+
+    localStorage.setItem("city name", JSON.stringify(cityEntered));
+    console.log("the city you entered is " + cityEntered);
+})
