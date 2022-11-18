4006cd22091ba9e9b3ff161e97e083ef6d24bd29
hmustain <hunter@bunkerbranding.com>
2022-11-18 11:48:17 -0600

    current weather/city is now dynamically reporting

--- assets/script.js
+++ assets/script.js
@@ -2,92 +2,131 @@ var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#cityname");
 var searchCityContainerEl = document.querySelector("#searchcity");
 var citySearch = document.querySelector("#citysearch");
+var cityLocation = document.querySelector("#citylocation");
+var currentTemp = document.querySelector("#current-temp");
+var currentWind = document.querySelector("#current-wind");
+var currentHum = document.querySelector("#current-hum");
+var weatherIcon = document.queryCommandValue("#weather-icon");
 var apiKey = "1c0a9400ca0f8243bdd42c0e2c421139";
-var currentDay = dayjs().format('MM/DD/YYYY');
-$('#1a').text(currentDay);
+var currentDay = dayjs().format("MM/DD/YYYY");
+// $("#1a").text(currentDay);
 
 console.log("today is " + currentDay);
 
 function getApi(cityEntered) {
-    // fetch request gets a list of all the repos for the node.js organization
-    // below is combination from going back to ins_demo act 3 and ask bcs told me to use string template literals
-    var url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityEntered}&appid=${apiKey}`;
-    console.log(url);
+  // fetch request gets a list of all the repos for the node.js organization
+  // below is combination from going back to ins_demo act 3 and ask bcs told me to use string template literals
+  var url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityEntered}&appid=${apiKey}`;
+  console.log(url);
 
-    fetch(url)
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data[0]);
+      cityLocation.textContent=data[0].name + " " + currentDay;
+      console.log("my api key is " + apiKey);
+
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+
+      var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+      fetch(weatherUrl)
         .then(function (response) {
-            return response.json();
+          return response.json();
         })
         .then(function (data) {
-            console.log(data[0]);
+          console.log(data);
 
-            console.log("my api key is " + apiKey);
+          currentTemp.textContent=" Temp: " + data.main.temp + " °F";
+          currentWind.textContent=" Wind Speed: " + data.wind.speed + " mph ";
+          currentHum.textContent=" Humidity: " + data.main.humidity + "% ";
+          console.log(
+            "the temperature for " +
+              cityEntered +
+              " is " +
+              data.main.temp +
+              " F°"
+          );
+          console.log(
+            "wind speeds for " +
+              cityEntered +
+              " is " +
+              data.wind.speed +
+              " mph, with gusts up to " +
+              data.wind.gust +
+              " mph "
+          );
+          console.log(
+            "Current humidity for " +
+              cityEntered +
+              " is " +
+              data.main.humidity +
+              " %"
+          );
 
-            var lat = data[0].lat;
-            var lon = data[0].lon;
+          var iconCode = data.weather[0].icon;
+          console.log(iconCode);
 
-            var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+          var iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+          console.log(iconUrl);
 
-            fetch(weatherUrl)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data);
-                    console.log("the temperature for " + cityEntered + " is " + data.main.temp + " F°");
-                    console.log("wind speeds for " + cityEntered + " is " + data.wind.speed + " mph, with gusts up to " + data.wind.gust + " mph ");
-                    console.log("Current humidity for " + cityEntered + " is " + data.main.humidity + " %");
+          console.log(weatherUrl);
 
+          console.log("the lat, lon for " + cityEntered + " is " + [lat, lon]);
+        });
 
-                    console.log(weatherUrl);
+      var fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
-                    console.log("the lat, lon for " + cityEntered + " is " + [lat, lon]);
-                });
+      fetch(fiveDayUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
 
-            var fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+          console.log(fiveDayUrl);
 
-            fetch(fiveDayUrl)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data);
+          console.log(data.list);
 
-                    console.log(fiveDayUrl);
-
-                });
 
         });
+    });
 }
 
 $("#citysearchbtn").on("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var cityEntered = $("#cityname").val().trim();
+  var cityEntered = $("#cityname").val().trim();
 
-    if (cityEntered) {
-        console.log("the city you entered is " + cityEntered);
-    } else {
-        alert("Please enter a valid City");
-    }
-
-    localStorage.setItem("city name", JSON.stringify(cityEntered));
+  if (cityEntered) {
     console.log("the city you entered is " + cityEntered);
-    searchCity();
+  } else {
+    alert("Please enter a valid City");
+  }
 
-    function searchCity() {
-        var prevCity = JSON.parse(localStorage.getItem("city name"));
-        var cityList = document.createElement("li");
-        cityList.setAttribute("class", "prev-city-item");
-        cityList.textContent = prevCity;
+  localStorage.setItem("city name", JSON.stringify(cityEntered));
+  console.log("the city you entered is " + cityEntered);
+  searchCity();
 
-        citySearch.append(cityList);
+  function searchCity() {
+    var prevCity = JSON.parse(localStorage.getItem("city name"));
+    var cityList = document.createElement("li");
+    cityList.setAttribute("class", "prev-city-item");
+    cityList.textContent = prevCity;
 
-        console.log("the previous city entered is " + prevCity);
-        console.log("the current city entered is " + cityEntered);
+    citySearch.append(cityList);
 
-        getApi(cityEntered);
-    }
+    console.log("the previous city entered is " + prevCity);
+    console.log("the current city entered is " + cityEntered);
+
+    getApi(cityEntered);
+  }
 });
 
-
+function currentWeather() {
+  var currentTemp = data.main.temp + " F° ";
+  console.log("the current temp is " + currentTemp);
+}
